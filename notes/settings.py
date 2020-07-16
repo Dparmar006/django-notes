ecb7be9375138ebc9a3c75c9902af64c2d14c303
Dixit Parmar <Dparmar006@users.noreply.github.com>
2020-07-16 22:29:42 +0530

    changed

--- notes/settings.py
+++ notes/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '0*1i8k=!#8tf!4h$)-xx&15961lybgf38zb=dy2rufhph!$ygw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['my-django-note-app.herokuapp.com']
+ALLOWED_HOSTS = ['my-django-note-app.herokuapp.com','127.0.0.1:8000']
 
 
 # Application definition
